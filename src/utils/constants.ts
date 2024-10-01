80959438322d57aa5c70dbc59929ab52365eb099
Thomas Eykamp <embergameslp@gmail.com>
2024-10-01 13:56:45 -0400

    improve miniNode location

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,21 +1,21 @@
-export const nodeRadius = 40
+export const nodeRadius = 50
 export const miniNodeRadius = 10
 
 export const miniNodeOffsets = {
   'top': {
     x: 0,
-    y: -nodeRadius
+    y: -nodeRadius + miniNodeRadius / 2
   },
   'right': {
-    x: nodeRadius,
+    x: nodeRadius - miniNodeRadius / 2,
     y: 0
   },
   'bottom': {
     x: 0,
-    y: nodeRadius
+    y: nodeRadius - miniNodeRadius / 2
   },
   'left': {
-    x: -nodeRadius,
+    x: -nodeRadius + miniNodeRadius / 2,
     y: 0
   },
 }
